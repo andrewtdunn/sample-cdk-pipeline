e60335bf7f219e7a57bae29b71c34994333a4d01
Andrew Dunn <atd2005@gmail.com>
2024-06-05 13:23:16 -0500

    change pipeline name

--- lib/sample-cdk-pipeline-stack.ts
+++ lib/sample-cdk-pipeline-stack.ts
@@ -14,7 +14,7 @@ export class SampleCdkPipelineStack extends cdk.Stack {
     super(scope, id, props);
 
     const pipeline = new CodePipeline(this, "Pipeline", {
-      pipelineName: "SampleCdkPipeline",
+      pipelineName: "SampleCdkPipelineTest",
       crossAccountKeys: true,
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.connection(
