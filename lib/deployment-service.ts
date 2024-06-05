71e285fea5866ac4b3c88fdb093dfbf399040c43
Andrew Dunn <atd2005@gmail.com>
2024-06-05 12:04:06 -0500

    add deployment

--- lib/deployment-service.ts
+++ lib/deployment-service.ts
@@ -5,7 +5,31 @@ import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
 import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 
-const path = "./resources/build";
+const path = "./react/build";
 
+export class DeploymentService extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
 
-export class DeploymentService extends Construct
+    const bucket = new Bucket(this, "ReactBucket", {
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    new BucketDeployment(this, "ReactBucketDeployment", {
+      sources: [Source.asset(path)],
+      destinationBucket: bucket,
+    });
+
+    const distribution = new Distribution(this, "Distribution", {
+      defaultBehavior: {
+        origin: new S3Origin(bucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+    });
+
+    new CfnOutput(this, "DistributionDomainName", {
+      value: distribution.domainName,
+    });
+  }
+}
