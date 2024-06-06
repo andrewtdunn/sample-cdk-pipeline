fb94573466a15f0d914b4a774ce2e070a4a65ea7
Andrew Dunn <atd2005@gmail.com>
2024-06-06 09:40:27 -0500

    update output dir

--- infra/lib/sample-cdk-pipeline-stack.ts
+++ infra/lib/sample-cdk-pipeline-stack.ts
@@ -15,7 +15,7 @@ export class SampleCdkPipelineStack extends cdk.Stack {
     super(scope, id, props);
 
     const pipeline = new CodePipeline(this, "Pipeline", {
-      pipelineName: "SampleCdkPipelineTest10",
+      pipelineName: "SampleCdkPipelineTest11",
       crossAccountKeys: true,
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.connection(
@@ -35,6 +35,7 @@ export class SampleCdkPipelineStack extends cdk.Stack {
           "npm run build",
           "npx cdk synth",
         ],
+        primaryOutputDirectory: "infra/cdk.out",
       }),
     });
 
