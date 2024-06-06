b4c0d016c1f5eaf098677d918392b425ec7e0df7
Andrew Dunn <atd2005@gmail.com>
2024-06-05 20:59:40 -0500

    fix build dir; update name

--- lib/sample-cdk-pipeline-stack.ts
+++ lib/sample-cdk-pipeline-stack.ts
@@ -8,7 +8,6 @@ import {
 } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 import { SampleCdkPipelineStage } from "./sample-cdk-pipeline-stage";
-import { BuildSpec } from "aws-cdk-lib/aws-codebuild";
 
 export class SampleCdkPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -19,14 +18,14 @@ export class SampleCdkPipelineStack extends cdk.Stack {
       crossAccountKeys: true,
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.connection(
-          "andrewtdunn/sample-cdk-pipeline",
+          "andrewtdunn/sample-cdk-pipeline8",
           "main",
           {
             connectionArn:
               "arn:aws:codestar-connections:us-east-1:637423577773:connection/78b54ada-1f46-4e0d-8b5c-572f1c8ee882",
           }
         ),
-        commands: ["npm ci", "npm run build", "npx cdk synth"],
+        commands: ["npm ci", "npm run build", "cdk synth"],
       }),
     });
 
